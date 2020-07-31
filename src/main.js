1f4328872f9e41e7b9f7b5902f63803d63e6a1be
yungtouch-sudo <yungtouch@yandex.ru>
2020-07-31 20:07:55 +0300

    2(1) webp+exports

--- src/main.js
+++ src/main.js
@@ -1,81 +1,14 @@
-"use strict";
-
 const FILM_COUNT = 5;
 
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
 
-const createUserRank = () => {
-  return (
-    `    <section class="header__profile profile">
-      <p class="profile__rating">Movie Buff</p>
-      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-    </section>`
-  );
-};
-
-const createSiteMenu = () => {
-  return (
-    `    <nav class="main-navigation">
-      <div class="main-navigation__items">
-        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-      </div>
-      <a href="#stats" class="main-navigation__additional">Stats</a>
-    </nav>`
-  );
-};
-
-const createSiteSorting = () => {
-  return (
-    `    <ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
-    </ul>`
-  );
-};
-
-const createFilms = () => {
-  return (
-    `    <section class="films">
-      <section class="films-list">
-        <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-        <div class="films-list__container">
-        </div>
-      </section>`
-  );
-};
-
-const createFilmCard = () => {
-  return (
-    `    <article class="film-card">
-      <h3 class="film-card__title">The Dance of Life</h3>
-      <p class="film-card__rating">8.3</p>
-      <p class="film-card__info">
-        <span class="film-card__year">1929</span>
-        <span class="film-card__duration">1h 55m</span>
-        <span class="film-card__genre">Musical</span>
-      </p>
-      <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
-        <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
-        <a class="film-card__comments">5 comments</a>
-        <form class="film-card__controls">
-          <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-          <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-          <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-        </form>
-    </article>`
-  );
-};
-
-const createButtonShowMore = () => {
-  return (
-    `<button class="films-list__show-more">Show more</button>`
-  );
-};
+import {createUserRank} from "./view/UserRank.js";
+import {createSiteMenu} from "./view/SiteMenu.js";
+import {createSiteSorting} from "./view/SiteSorting.js";
+import {createFilms} from "./view/Films.js";
+import {createFilmCard} from "./view/FilmCard.js";
+import {createButtonShowMore} from "./view/ButtonShowMore.js";
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -87,10 +20,11 @@ render(siteMainElement, createSiteSorting());
 render(siteMainElement, createFilms());
 
 const filmsElement = siteMainElement.querySelector(`.films`);
-const filmListContainer = filmsElement.querySelector(`.films-list__container`);
+let filmListContainer = filmsElement.querySelector(`.films-list__container`);
 
 for (let i = 0; i < FILM_COUNT; i++) {
   render(filmListContainer, createFilmCard());
 }
 render(filmsElement, createButtonShowMore());
+
 
