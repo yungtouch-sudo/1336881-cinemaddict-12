1fe3c7c4d17973aa930d49f29e083be75443e7f9
yungtouch-sudo <yungtouch@yandex.ru>
2020-09-29 16:18:39 +0400

    rdy

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  AUTHORIZATION: `Basic KJBjjakljaojs655hgat`,
+  END_POINT: `https://12.ecmascript.pages.academy/cinemaddict/`,
+  STORE_PREFIX: `cinemaadict-localstorage`,
+  STORE_VER: `v12`,
+};
